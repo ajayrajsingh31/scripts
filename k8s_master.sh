f1f77738f2aa69dde58954ef60f5a5b809583869
ajayrajsingh31 <121920229+ajayrajsingh31@users.noreply.github.com>
2024-08-30 11:05:40 +0530

    Update and rename k8s.sh to k8s_master.sh

--- k8s_master.sh
+++ k8s_master.sh
@@ -6,3 +6,8 @@ sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
