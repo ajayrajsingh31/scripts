e134fd8900f3ef1fc28bab4090fca7a8cc726bdb
ajayrajsingh31 <121920229+ajayrajsingh31@users.noreply.github.com>
2024-08-30 10:51:07 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,9 @@
+sudo su
+swapoff -a
+
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo apt install docker-buildx
+sudo usermod -aG docker $USER && newgrp docker
+sudo chmod 777 /var/run/docker.sock
