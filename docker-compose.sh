04228369de81c8e90c576bee9ee78b005e40f0f4
ajayrajsingh31 <121920229+ajayrajsingh31@users.noreply.github.com>
2024-09-02 11:31:27 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,3 +2,8 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
  
 sudo chmod +x /usr/local/bin/docker-compose
  
+docker-compose up --build
+
+docker compose down 
+
+sudo rm -rf ./app  # Be cautious with this command as it deletes the MySQL data directory
